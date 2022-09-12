04d41d394ee1b65eb1f99892c626827786374f14
mtellezjis <70731290+mtellezjis@users.noreply.github.com>
2022-09-12 14:29:03 +0700

    Sep 12

---
+++ Unit2/js1.js
@@ -0,0 +1,20 @@
+//this is a js file
+alert("Hello JS - Loaded from my own JS file");
+
+//this function is for the button
+function test1(){
+    alert("The button was pressed");
+}
+
+function test2() {
+    
+}
+
+
+
+
+function test3(){
+
+}
+
+alert("This is written after the function");
---
+++ Unit2/js2.js
@@ -0,0 +1,11 @@
+function option1(){
+    alert("You won 500!");
+}
+
+function option2(){
+    alert("You won 100!");
+}
+
+function option3(){
+    alert("you lose all your point! :/");
+}
